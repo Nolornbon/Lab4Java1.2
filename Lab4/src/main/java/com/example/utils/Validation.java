1d4a10c1711066453fb3734058952c31b0b4b2f3
Misha Smishnyi <89969944+Nolornbon@users.noreply.github.com>
2023-06-11 15:45:39 +0300

    Update1 Validation.java

--- Lab4/src/main/java/com/example/utils/Validation.java
+++ Lab4/src/main/java/com/example/utils/Validation.java
@@ -1,36 +1,12 @@
 package com.example.utils;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+@UtilityClass
 public class Validation {
 
-/*
-    public static void validateInput(double[] dividend, double[] divisor) {
-        if (divisor.length > dividend.length) {
-            throw new IllegalArgumentException("Divisor length cannot be greater than dividend length.");
-
-
-        }
-
-        if (divisor.length == 0) {
-            throw new IllegalArgumentException("Divisor cannot be empty.");
-
-        }
-        boolean allZeroes = true;
-        for (double value : divisor) {
-            if (value != 0.0) {
-                allZeroes = false;
-                break;
-            }
-        }
-        if (allZeroes) {
-            throw new IllegalArgumentException("Divisor cannot contain only zero values.");
-
-        }
-    }
-}
-*/
 
 
     public static void validateInput(double[] dividend, double[] divisor) {
